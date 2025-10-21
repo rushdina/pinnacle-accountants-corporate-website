e1022b5db316e494db4f9e2e9b8be4746bc7dd44
rushdina <redredrushrush@gmail.com>
2025-10-21 14:22:58 +0800

    Made changes to images, content and colours

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Function to handle navbar background change on scroll
-window.onscroll = function() {
+/*window.onscroll = function() {
     const navbar = document.querySelector('.custom-navbar');
     if (window.scrollY > 50) {
         navbar.classList.add('navbar-scrolled'); // Add scrolled class
     } else {
             navbar.classList.remove('navbar-scrolled');
     }
-};
+};*/
 
 // Duration for each testimonial
-var myCarousel = document.querySelector('#testimonialCarousel');
+var myCarousel = document.querySelector("#testimonialCarousel");
 var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 6000, // 6 seconds duration for each testimonial
-    wrap: true
+  interval: 6000, // 6 seconds duration for each testimonial
+  wrap: true,
 });
